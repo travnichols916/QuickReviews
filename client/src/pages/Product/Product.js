d146c2697e3ac8d01fbd7b2795a608f4dcbd39f4
Okens <Ng.Eric314@gmail.com>
2022-07-04 22:17:54 -0700

    Adding review form for product page.

--- client/src/pages/Product/Product.js
+++ client/src/pages/Product/Product.js
@@ -8,11 +8,15 @@ import {
   Rating,
   Stack,
   Tab,
+  TextField,
+  Button
 } from '@mui/material'
 
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
+import { 
+  TabContext,
+  TabList,
+  TabPanel 
+} from '@mui/lab';
 // import StarIcon from '@mui/icons-material/Star';
 import { styled  } from '@mui/material/styles';
 import { lightBlue } from '@mui/material/colors';
@@ -33,15 +37,8 @@ const ContainerStyled = styled(Container)(({ theme }) => ({
   },
 }));
 
-// const Item = styled(Paper)(({ theme }) => ({
-//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//   ...theme.typography.body2,
-//   padding: theme.spacing(0.5),
-//   textAlign: 'center',
-//   color: theme.palette.text.secondary,
-// }));
-
 const labels = {
+  0: 'Strong Dislike',
   0.5: 'Strong Dislike',
   1: 'Strong Dislike',
   1.5: 'Dislike',
@@ -63,16 +60,127 @@ const Product = () => {
   const [userReview, setUserReview] = useState({});
   const [productData, setProductData] = useState({});
   const [tabReviewValue, setTabReviewValue] = React.useState('1');
+  const [reviewerStarValue, setReviewerStarValue] = React.useState(0.5);
+  const [reviewerStarHover, setReviewerStarHover] = React.useState(-1);
+  const [titleValue, setTitleValue] = React.useState('');
+  const [commentValue, setCommentValue] = React.useState('');
+  const [submittedValue, setSubmittedValue] = React.useState(false);
+
+  const [dataReviews, setDataReviews] = React.useState([
+    {
+      username: "MissingNo.",
+      rating: 1,
+      title: "Lorem ipsum dolor sit amet.",
+      comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+      username: "Tom",
+      rating: 4,
+      title: "Great bait.",
+      comment: "Worked as intended to lure stuff."
+    },
+    {
+      username: "Jerry",
+      rating: 2,
+      title: "Definitely did not like product.",
+      comment: "Did not taste like cheese."
+    },
+    {
+      username: "Rock",
+      rating: 4.5,
+      title: "Great product.",
+      comment: "Amazingly hard cheese."
+    }
+  ]);
 
   const handleTabReviewChange = (event, newValue) => {
+    event.preventDefault();
     setTabReviewValue(newValue);
   };
 
-  const [reviewerValue, setReviewerValue] = React.useState(2);
-  const [reviewerHover, setReviewerHover] = React.useState(-1);
+  const buildReviews = (reviews) => {
+    return (
+      <Stack spacing={4} sx={{m: 1}}>
+        {reviews.map((review) => {
+          return(
+            <Stack spacing={0.5} sx={{borderTop: '1px solid #eee', paddingTop: '1rem'}}>
+              <Box component="span">{review.username}</Box>
+              <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <Rating name="half-rating-read" value={review.rating} precision={0.5} readOnly />
+                <Box component="span" sx={{ml: 1, fontWeight: 'bold'}}>{review.title}</Box>
+              </Box>
+              <Box component="p">{review.comment}</Box>
+            </Stack>
+            )
+        })}
+      </Stack>
+    )
+  };
 
-  useEffect(() => {
-  }, [productData])
+  const handleReviewFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submit button titleValue: ", titleValue);
+    console.log("Submit button reviewerStarValue: ", reviewerStarValue);
+    console.log("Submit button commentValue: ", commentValue)
+  }
+
+  const buildReviewerForm = () => {
+    return (
+      <Stack spacing={1}>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '60ch' },
+            '& .MuiButton-root': { width: '15ch' }
+          }}
+          onSubmit={handleReviewFormSubmit}
+          noValidate
+          autoComplete="off"
+        >
+          <Stack spacing={0}>
+            <TextField
+              id="reviewFormTitle"
+              label="Title"
+              placeholder="Title"
+              multiline
+              
+              onChange={(event) => setTitleValue(event.target.value)}
+            />
+            <Box sx={{m: 1, display: 'flex', alignItems: 'center'}}>
+              <Rating name="half-rating" defaultValue={reviewerStarValue} precision={0.5} 
+                getLabelText={getLabelText}
+                onChange={(event, newValue) => {
+                  setReviewerStarValue(newValue);
+                }}
+                onChangeActive={(event, newHover) => {
+                  setReviewerStarHover(newHover);
+                }}
+                // emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+              />
+              {reviewerStarValue !== null && (
+                <Box sx={{ ml: 2, mb: '0.5rem', alignItems: 'center' }}>{labels[reviewerStarHover !== -1 ? reviewerStarHover : reviewerStarValue]}</Box>
+              )}
+            </Box>
+            <TextField
+              id="reviewFormComment"
+              label="Comment"
+              multiline
+              rows={4}
+              placeholder="Comment"
+              onChange={(event) => setCommentValue(event.target.value)}
+            />
+            <Button sx={{m: 1}} variant="contained" type="submit">Submit</Button>
+            {submittedValue === true && (<Box component="span">Submitted</Box>)}
+          </Stack>
+        </Box>
+      </Stack>
+    )
+
+  }
+
+
+  // useEffect(() => {
+  // }, [productData])
 
   return (
     <BoxBackground>
@@ -106,8 +214,8 @@ const Product = () => {
                   </Box>
                   <Box sx={{display: 'flex', alignItems: 'center'}}>
                     <Rating name="half-rating-read" defaultValue={4.5} precision={0.5} size="large" readOnly />
-                    {reviewerValue !== null && (
-                        <Box sx={{ ml: 2, alignItems: 'center' }}>{labels[reviewerValue]}</Box>
+                    {reviewerStarValue !== null && (
+                        <Box sx={{ ml: 1, alignItems: 'center' }}>{labels[reviewerStarValue]}</Box>
                       )}
                   </Box>
                   <Box component="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -120,11 +228,7 @@ const Product = () => {
               justifyContent="center"
               alignItems="center"
             >
-              <Grid item sx={gridStyles} xs={12} md={8} lg={8}>
-                <Box component="h2" sx={{textAlign: 'center'}}>Reviews</Box>
-              </Grid>
               <Grid item sx={gridStyles} xs={12} >
-               
                 <Box sx={{ width: '100%', typography: 'body1' }}>
                   <TabContext value={tabReviewValue}>
                     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
@@ -134,35 +238,10 @@ const Product = () => {
                       </TabList>
                     </Box>
                     <TabPanel value="1">
-                      <Stack spacing={1}>
-                        <Box sx={{display: 'flex', alignItems: 'center'}}>
-                          <Rating name="half-rating-read" defaultValue={5} precision={0.5} readOnly />
-                          {reviewerValue !== null && (
-                              <Box sx={{ ml: 2, alignItems: 'center' }}>{labels[reviewerValue]}</Box>
-                            )}
-                        </Box>
-                        <Box component="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </Box>
-                      </Stack>
+                      {buildReviews(dataReviews)}
                     </TabPanel>
                     <TabPanel value="2">
-                      <Stack spacing={1}>
-                        <Box sx={{display: 'flex', alignItems: 'center'}}>
-                          <Rating name="half-rating" defaultValue={2.5} precision={0.5} 
-                            getLabelText={getLabelText}
-                            onChange={(event, newValue) => {
-                              setReviewerValue(newValue);
-                            }}
-                            onChangeActive={(event, newHover) => {
-                              setReviewerHover(newHover);
-                            }}
-                            // emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
-                          />
-                          {reviewerValue !== null && (
-                            <Box sx={{ ml: 2, mb: '0.5rem', alignItems: 'center' }}>{labels[reviewerHover !== -1 ? reviewerHover : reviewerValue]}</Box>
-                          )}
-                        </Box>
-                      </Stack>
+                      {buildReviewerForm()}
                     </TabPanel>
                   </TabContext>
                 </Box>
