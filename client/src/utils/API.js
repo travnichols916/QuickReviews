319f06b4d8fcbe4a0c9b0ee7f302dfa178fcdc8e
Maribel <lwang79@zoho.com>
2022-07-10 14:09:32 -0700

    moved search function to the API.js, and then made it so that the search saves to the local storage

---
+++ client/src/utils/API.js
@@ -0,0 +1,34 @@
+// make a search to google books api
+// https://www.googleapis.com/books/v1/volumes?q=harry+potter
+export const searchGoogleBooks = (query) => {
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("items: ", data)
+      const resultsData = data.items.map((product) => {
+      const { id, volumeInfo } = product;
+      const { authors, title, description, averageRating, imageLinks, industryIdentifiers } = volumeInfo;
+      let isbn = "";
+      if(industryIdentifiers.length > 1) {
+        isbn = industryIdentifiers.filter(industryIdentifier => industryIdentifier.type === "ISBN_13")[0].identifier
+      }
+
+      return {
+        bookId: id,
+        authors: authors || ['No author to display'],
+        title: title,
+        description: description,
+        averageRating: averageRating,
+        image: imageLinks?.thumbnail || '',
+        isbn: isbn
+      }
+    });
+
+    console.log("resultsData", resultsData)
+    localStorage.setItem('resultsData', JSON.stringify(resultsData))
+    })
+
+  
+};
